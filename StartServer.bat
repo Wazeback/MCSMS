de026e02c8b18474b3b9d705dde71a8c55c796ab
Wazeback <david.lelystad@gmail.com>
2023-06-08 22:13:32 +0200

    Server Start

--- StartServer.bat
+++ StartServer.bat
@@ -15,12 +15,8 @@ set "SERVER_DIR=%~dp0"
     git-portable\bin\git.exe add .
     git-portable\bin\git.exe commit -m "Server Start"
     git push origin master
-    start "" /B run.bat
-    echo Server has started up.
-exit /b
-
-
-:stop_server
+    java-portable\jdk-20.0.1\bin\java.exe -Xmx1024M -Xms1024M -jar "server.jar"
+    echo Server wil start up.
     git-portable\bin\git.exe pull origin master
     if exist "%SERVER_DIR%\server.lock" (
         echo stop > "%SERVER_DIR%\stop.txt"
@@ -41,13 +37,4 @@ exit /b
 
 REM Main script logic
 call :start_server
-
-REM Wait for the "stop" command in the text box
-:wait_for_stop
-set /p command=
-if /I "%command%"=="stop" (
-    call :stop_server
-    exit /b
-)
-goto wait_for_stop
 pause
